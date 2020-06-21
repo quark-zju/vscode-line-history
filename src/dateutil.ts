f0e27307dad4a9e28372609124e453145a2eb4cb
Jun Wu <quark@lihdd.net>
2020-06-20 19:53:52 -0700

    Use shorter text for displayTime

--- src/dateutil.ts
+++ src/dateutil.ts
@@ -1,21 +1,26 @@
+
 // Render time (`ago` in millis) in relative form if it's closer to `now`.
-let displayTime = (ago: number, now: number | null = null): string => {
-    const msPerMinute = 60 * 1000;
-    const msPerHour = msPerMinute * 60;
-    const msPerDay = msPerHour * 24;
-    const msPerWeek = msPerDay * 7;
-    var elapsed = (now || Date.now()) - ago;
-    if (elapsed < msPerMinute * 2) {
-        return "just now";
-    } else if (elapsed < msPerHour * 2) {
-        return Math.round(elapsed / msPerMinute) + ' minutes ago';
-    } else if (elapsed < msPerDay * 2) {
-        return Math.round(elapsed / msPerHour) + ' hours ago';
-    } else if (elapsed < msPerWeek * 2) {
-        return Math.round(elapsed / msPerDay) + ' days ago';
-    } else {
-        return new Date(ago).toLocaleString();
-    }
-};
+let displayTime = (() => {
+    // @ts-ignore
+    let format = new Intl.DateTimeFormat(undefined, { dateStyle: 'short', timeStyle: 'short' }).format;
+    return (ago: number, now: number | null = null): string => {
+        const msPerMinute = 60 * 1000;
+        const msPerHour = msPerMinute * 60;
+        const msPerDay = msPerHour * 24;
+        const msPerWeek = msPerDay * 7;
+        var elapsed = (now || Date.now()) - ago;
+        if (elapsed < msPerMinute * 2) {
+            return "just now";
+        } else if (elapsed < msPerHour * 2) {
+            return Math.round(elapsed / msPerMinute) + ' minutes ago';
+        } else if (elapsed < msPerDay * 2) {
+            return Math.round(elapsed / msPerHour) + ' hours ago';
+        } else if (elapsed < msPerWeek * 2) {
+            return Math.round(elapsed / msPerDay) + ' days ago';
+        } else {
+            return format(new Date(ago));
+        }
+    };
+})();
 
 export { displayTime };
